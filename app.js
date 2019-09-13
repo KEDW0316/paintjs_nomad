cf9ed70aa1884f0346f008a43ed4b3e5989fed49
unknown <dks01972@gmail.com>
2019-09-13 20:22:37 +0900

    PaintJS_nomad Draw Line Project

--- app.js
+++ app.js
@@ -1,21 +1,47 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
+
+canvas.width = 700;
+canvas.height = 700;
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
+
+
 let painting = false;
 function stopPainting(){
     painting=false;
 }
+
+function startPainting(){
+    painting = true;
+}
 function onMouseMove(event){
     const x = event.offsetX;
     const y = event.offsetY;
-}
-function onMouseDown(event){
-    painting = true;
+    
+    if(painting){
+        ctx.lineTo(x,y);
+        ctx.stroke();
+    } else{
+        ctx.beginPath();
+        ctx.moveTo(x,y);
+    }
 }
 function onMouseUp(event){
     stopPainting();
 }
+
+function changeColor(event){
+    const selectedColor = event.target.style.backgroundColor;
+    ctx.strokeStyle = selectedColor;
+    console.log(ctx.strokeStyle);
+}
 if(canvas){
    canvas.addEventListener("mousemove",onMouseMove);
-   canvas.addEventListener("mousedown", onMouseDown);
-   canvas.addEventListener("mouseup", onMouseUp);
+   canvas.addEventListener("mousedown", startPainting);
+   canvas.addEventListener("mouseup", stopPainting);
    canvas.addEventListener("mouseleave", stopPainting);
 } 
+
+Array.from(colors).forEach(color => color.addEventListener("click",changeColor));
